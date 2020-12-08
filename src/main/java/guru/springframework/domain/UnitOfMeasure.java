e7a0f6b7ab2e57d2e11dc3f9d9212f8eba0bc540
EduardoReis7 <53585326+EduardoReis7@users.noreply.github.com>
2020-12-08 17:42:49 -0300

    one-to-many

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,12 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String uom;
+}
